27290a48c5eff6dda56108cc63ec7f0412d1bfcf
nsheo <nsheo@ntels.com>
2021-12-06 17:03:25 +0900

    test

--- lib/embulk/output/verticacsv/output_thread.rb
+++ lib/embulk/output/verticacsv/output_thread.rb
@@ -27,13 +27,13 @@ module Embulk
           current_time = Time.now
           page.each do |record|
             csv_data << to_csv(record)
-            unless @task['add_time_col'].nil? 
-              csv_data << @task['delimiter_str'] << @converters[@task['add_time_col']].call(current_time) 
+            unless @task['load_time_col'].nil? 
+              csv_data << @task['delimiter_str'] << @converters[@task['load_time_col']].call(current_time) 
             end
-            Embulk.logger.debug { "embulk-output-verticacsv: Check data 2 #{csv_data}" }
           end
+          Embulk.logger.debug { "embulk-output-verticacsv: Check data 2 #{csv_data}" }
           @mutex.synchronize do
-            @output_threads[@current_index].enqueue(csv_data, table_col_info)
+            @output_threads[@current_index].enqueue(csv_data)
             @current_index = (@current_index + 1) % @size
           end
         end
